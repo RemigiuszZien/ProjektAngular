daa34ac2ae682258bb07e6061ec849ab8f8a0d09
RemigiuszZien <RemigiuszZien@users.noreply.github.com>
2025-08-21 13:42:50 +0200

    poprawa wyswietlania afiksow itemow

--- src/app/shared/components/build/build.ts
+++ src/app/shared/components/build/build.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, input, output, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, output, ChangeDetectionStrategy, signal, effect } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Build as BuildInterface } from '../../interfaces/build';
 import { Equipment } from '../equipment-grid/equipment-grid';
+import { ItemService, Item } from '../../services/item.service';
 
 @Component({
   selector: 'app-build',
@@ -13,4 +14,15 @@ import { Equipment } from '../equipment-grid/equipment-grid';
 export class Build {
   build = input<BuildInterface | undefined>(undefined);
   close = output<void>();
+  private itemService = new ItemService();
+  selectedItem = signal<Item | null>(null);
+
+  showItemMods(name: string) {
+    this.itemService.getItemById(name).subscribe(item => {
+      this.selectedItem.set(item ?? null);
+    });
+  }
+  closeItemModal() {
+    this.selectedItem.set(null);
+  }
 }
